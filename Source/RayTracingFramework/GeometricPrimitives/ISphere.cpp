adbf51f78294dd367adc14d30b04b9a4aad7a656
Unknown <tomchescreations@gmail.com>
2017-11-03 16:50:56 +0000

    Implemented Ray-Sphere Intersection

--- Source/RayTracingFramework/GeometricPrimitives/ISphere.cpp
+++ Source/RayTracingFramework/GeometricPrimitives/ISphere.cpp
@@ -49,6 +49,45 @@ int RayTracingFramework::ISphere::testRaySphereCollision(glm::vec4 origin_local,
 	, float &t1, glm::vec4& collision_Point1, glm::vec4& collision_Normal1
 	, float &t2, glm::vec4& collision_Point2, glm::vec4& collision_Normal2) {
 
+	//Origin of sphere.
+	glm::vec4 O = glm::vec4(0, 0, 0, 1);
+
+	//Sphere origin to Ray origin.
+	glm::vec4 OP0 = origin_local - O;
+
+	//Calculate quadratic coefficients.
+	float a = glm::dot(direction_local, direction_local);
+	float b = 2.0f * glm::dot(direction_local, OP0);
+	float c = glm::dot(OP0, OP0) - radius * radius;
+
+	//Calculate discriminant.
+	float discriminant = b*b - 4.0f * a * c;
+	//Return t = infinite (no collision) if discriminant < 0.
+	//Negative discriminant means imaginary roots.
+	//Which tells us there's no intersection.
+	if (discriminant < 0.0f)
+		return 0;
+
+	//Find roots.
+	t1 = (-1.0f * b + glm::sqrt(discriminant)) / (2.0f * a);
+	t2 = (-1.0f * b - glm::sqrt(discriminant)) / (2.0f * a);
+
+	//Use roots to calculate collision points.
+	collision_Point1 = origin_local + direction_local * t1;
+	collision_Point2 = origin_local + direction_local * t2;
+
+	//Use collision points to work out normals.
+	glm::vec4 OQ_1 = collision_Point1 - O;
+	float OQ_mag_1 = glm::length(OQ_1);
+	collision_Normal1 = OQ_1 / OQ_mag_1;
+	glm::vec4 OQ_2 = collision_Point2 - O;
+	float OQ_mag_2 = glm::length(OQ_2);
+	collision_Normal2 = OQ_2 / OQ_mag_2;
 	
-	return 0;
+	//A discriminant of exactly zero means there's one point of intersection.
+	if (discriminant == 0.0f)
+		return 1;
+
+	//>0 means 2 points of intersection.
+	return 2;
 }
